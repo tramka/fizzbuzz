b9f7a6c514467b718c52853fae930c0eb1413728
tramka <37137459+tramka@users.noreply.github.com>
2018-11-14 19:23:16 +0100

    Added docstring to the function.

--- fz.py
+++ fz.py
@@ -1,4 +1,20 @@
 def fizzbuzz(n):
+    '''
+    Returns the result of the game. If number is not divisible by 3 or 5, 
+    returns provided number. If number is divisible by 3, returns 'fizz',
+    if number is divisible by 5, returns 'buzz'. If number is divisible by 3
+    and 5, returns 'fizzbuzz'.
+    
+    Parameters
+    ----------
+    n: <int>
+        Any number.
+        
+    Returns
+    -------
+    <str>
+        The result of the game.
+    '''
     if not isinstance(n, int):
         raise TypeError()
     if n % 15 == 0:
